bcc5843b2a74d6b51601ad3029cc2edd1ab6a86f
= <terrahen@web.de>
2020-04-21 17:50:06 +0200

    assets

--- static/assets.js
+++ static/assets.js
@@ -7,19 +7,22 @@ const Downloader = require('downloadtools/src/Downloader');
 const manager = new Downloader();
 
 function download(file, target) {
-  FS.mkdirSync(Path.join(process.cwd(), target));
+  const output = Path.join(process.cwd(), target);
+  if (!FS.existsSync(output)) {
+    FS.mkdirSync(output);
+  }
   if (Path.isAbsolute(file)) {
     data = require(file);
   } else {
     data = require(Path.join(process.cwd(), file));
   }
-  const bulk = manager.createMulti(data).setCWD(target);
+  const bulk = manager.createMulti(data).setCWD(output);
 
   return bulk.execute();
 }
 
 (async function () {
-  await download('static/data/narrator.json', 'static/downloads/narrator');
-  await download('static/data/sounds.json', 'static/downloads/sounds');
-  await download('static/data/videos.json', 'static/downloads/videos');
+  await download('static/data/narrator.json', 'static/downloads/narrator').promise;
+  await download('static/data/sounds.json', 'static/downloads/sounds').promise;
+  await download('static/data/videos.json', 'static/downloads/videos').promise;
 })();
