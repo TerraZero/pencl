d91aa23de74e698dbe8b224166ae640b44d206f6
= <terrahen@web.de>
2020-04-21 17:41:57 +0200

    assets

--- static/assets.js
+++ static/assets.js
@@ -1,11 +1,13 @@
 #!/usr/bin/env node
 
 const Path = require('path');
+const FS = require('fs');
 const Downloader = require('downloadtools/src/Downloader');
 
 const manager = new Downloader();
 
 function download(file, target) {
+  FS.mkdirSync(Path.join(process.cwd(), target));
   if (Path.isAbsolute(file)) {
     data = require(file);
   } else {
@@ -17,7 +19,7 @@ function download(file, target) {
 }
 
 (async function () {
-  // await download('static/data/narrator.json', 'static/downloads/narrator');
-  // await download('static/data/narrator.json', 'static/downloads/narrator');
+  await download('static/data/narrator.json', 'static/downloads/narrator');
+  await download('static/data/sounds.json', 'static/downloads/sounds');
   await download('static/data/videos.json', 'static/downloads/videos');
 })();
